8a570c201aadaba33e78302ec92c53299dea61e3
Chris-Am <c.aranguizm@outlook.com>
2023-06-07 13:49:57 -0400

    :art: improving code according copilot chat

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-const Map<String, Color> namedColorList = <String, Color>{
-  'Red Accent': Colors.redAccent,
-  'Deep Orange': Colors.deepOrange,
-  'Blue Accent': Colors.blueAccent,
-  'Blue Grey': Colors.blueGrey,
-  'tealAccent': Colors.tealAccent,
-  'Purple Accent': Colors.purpleAccent,
-  'Deep Purple': Colors.deepPurple,
-  'Deep Purple Accent': Colors.deepPurpleAccent,
-  'Light Blue Accent': Colors.lightBlueAccent,
-  'Pink Accent': Colors.pinkAccent,
+final Map<String, Color> appColorPalette = <String, Color>{
+  'Red Accent': const Color(0xFFFF5252),
+  'Deep Orange': const Color(0xFFFF5722),
+  'Blue Accent': const Color(0xFF2979FF),
+  'Blue Grey': const Color(0xFF607D8B),
+  'Teal Accent': const Color(0xFF009688),
+  'Purple Accent': const Color(0xFF9C27B0),
+  'Deep Purple': const Color(0xFF673AB7),
+  'Deep Purple Accent': const Color(0xFF7C4DFF),
+  'Light Blue Accent': const Color(0xFF03A9F4),
+  'Pink Accent': const Color(0xFFE91E63),
 };
 
 class GlobalAppTheme {
@@ -20,18 +20,15 @@ class GlobalAppTheme {
   GlobalAppTheme({
     this.isDarkMode = false,
     this.selectedColor = 0,
-  })  : assert(
-          selectedColor >= 0,
-          'Selected color must be greater than or equal to 0',
-        ),
-        assert(
-          selectedColor < namedColorList.length,
-          'Selected color must be in range of ${namedColorList.length - 1}',
+  }) : assert(
+          selectedColor >= 0 && selectedColor < appColorPalette.length,
+          'Selected color must be between 0 and ${appColorPalette.length - 1}',
         );
+
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
         brightness: !isDarkMode ? Brightness.light : Brightness.dark,
-        colorSchemeSeed: namedColorList.values.toList()[selectedColor],
+        colorSchemeSeed: appColorPalette.values.toList()[selectedColor],
         appBarTheme: const AppBarTheme(centerTitle: false),
       );
 
--- lib/config/theme/app_theme_provider.dart
+++ lib/config/theme/app_theme_provider.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movies_app/config/theme/app_theme.dart';
 
-final Provider<Map<String, Color>> colorListProvider =
-    Provider<Map<String, Color>>((ref) => namedColorList);
+// Define a provider that provides a map of named colors
+final Provider<Map<String, Color>> namedColorListProvider =
+    Provider<Map<String, Color>>((ref) => appColorPalette);
 
+// Define a state notifier provider that provides an instance of GlobalAppThemeNotifier and GlobalAppTheme
 final StateNotifierProvider<GlobalAppThemeNotifier, GlobalAppTheme>
     globalAppThemeProvider =
     StateNotifierProvider<GlobalAppThemeNotifier, GlobalAppTheme>(
   (ref) => GlobalAppThemeNotifier(),
 );
 
+// Define a state notifier that manages the global app theme
 class GlobalAppThemeNotifier extends StateNotifier<GlobalAppTheme> {
+  // Initialize the state with a default global app theme
   GlobalAppThemeNotifier() : super(GlobalAppTheme());
-  void toggleDarkTheme() {
+
+  // Toggle the dark mode of the global app theme
+  void toggleDarkMode() {
     state = state.copyWith(isDarkMode: !state.isDarkMode);
   }
 
-  void toggleSelectedColor(int colorIndex) {
-    print(colorIndex);
+  // Set the selected color of the global app theme
+  void setSelectedColor(int colorIndex) {
     state = state.copyWith(selectedColor: colorIndex);
   }
 }
