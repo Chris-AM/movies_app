86dfb22b370a3935e16882176c259f5ce3284b63
Chris-Am <c.aranguizm@outlook.com>
2023-06-07 21:45:05 -0400

    :package: adding dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 //* Flutter Imports
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 //* Own Imports
 import 'package:movies_app/config/routers/app_router.dart';
 import 'package:movies_app/config/theme/app_theme.dart';
 import 'package:movies_app/config/theme/app_theme_provider.dart';
 
-void main() => runApp(const ProviderScope(child: MoviesApp()));
+// The main function is the entry point of the app.
+Future<void> main() async {
+  // Load environment variables from a .env file.
+  await dotenv.load(fileName: '.env');
+  // Start the app by running the MoviesApp widget inside a ProviderScope.
+  runApp(
+    const ProviderScope(
+      child: MovieApp(),
+    ),
+  );
+}
 
-class MoviesApp extends ConsumerWidget {
-  const MoviesApp({super.key});
+// The MoviesApp class is a ConsumerWidget that builds the app UI based on the current state of the app.
+class MovieApp extends ConsumerWidget {
+  const MovieApp({Key? key}) : super(key: key);
 
+  // The build method is called whenever the app state changes.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Get the current app theme from the globalAppThemeProvider provider.
     final GlobalAppTheme globalAppTheme = ref.watch(globalAppThemeProvider);
+    // Return a MaterialApp.router widget that provides routing functionality for the app.
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
