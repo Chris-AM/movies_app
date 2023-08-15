89d4bed3c9e164d3547fc847af9211d3e28919bd
Chris-Am <c.aranguizm@outlook.com>
2023-08-15 13:10:16 -0400

    :wrench: adding base url from nest project

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,5 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String tmdbKey = dotenv.get('TMDB_KEY');
+  // static String urlBase = 'http://api.themoviedb.org/3';
+  static String urlBase = dotenv.get('BASE_URL');
+
+  static Dio globalDio = Dio(
+    BaseOptions(
+      baseUrl: urlBase,
+      queryParameters: {
+        'api_key': tmdbKey,
+      },
+    ),
+  );
 }
